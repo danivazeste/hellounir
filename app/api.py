1a9801c8334ccc02631b570e85974bdc0bef52ef
danivazeste <168636081+danivazeste@users.noreply.github.com>
2024-05-05 19:29:46 +0200

    Update api.py multiply divide

--- app/api.py
+++ app/api.py
@@ -31,3 +31,25 @@ def substract(op_1, op_2):
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    try:            
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        # Aquí gestionamos el error de dividir por 0.
+        # Lanzamos el mensaje de que la división entre 0 no está permitida.
+        if(num_2 == 0):
+            return ("Divide by zero not allowed", http.client.NOT_ACCEPTABLE, HEADERS)
+
+        return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
